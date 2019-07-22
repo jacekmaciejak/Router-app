032c5d1d46fcec03636e05cd06d64be626408845
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-07-22 20:47:10 +0200

    Added Header component with images drawing

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,7 +1,17 @@
 import * as React from "react";
+import { withRouter } from "react-router-dom";
+import "../styles/Header.css";
+import img1 from "../images/img1.jpg";
+import img2 from "../images/img2.jpg";
+import img3 from "../images/img3.jpg";
 
 const Header = () => {
-  return <div>Header</div>;
+  const images = [img1, img2, img3];
+  const index = Math.floor(Math.random() * 3);
+
+  const img = images[index];
+
+  return <img src={img} alt="Boat" />;
 };
 
-export default Header;
+export default withRouter(Header);
