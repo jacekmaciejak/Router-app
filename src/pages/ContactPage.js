545fa3023495caf02e7ed8d4c4f6a3b7fe31fa03
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-07-24 20:11:05 +0200

    Contact component with protection method against data loss

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,7 +1,43 @@
 import * as React from "react";
+import "../styles/ContactPage.css";
+import { Prompt } from "react-router-dom";
 
-const ContactPage = () => {
-  return <div>Kontakt</div>;
-};
+class ContactPage extends React.Component {
+  state = {
+    value: ""
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({
+      value: ""
+    });
+  };
+
+  handleChange = e => {
+    this.setState({
+      value: e.target.value
+    });
+  };
+  render() {
+    return (
+      <div className="contact">
+        <form onSubmit={this.handleSubmit}>
+          <h3>Napisz cos do nas</h3>
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange}
+            placeholder="Wpisz wiadomosc..."
+          />
+          <button>Wyslij</button>
+        </form>
+        <Prompt
+          when={this.state.value}
+          message="Nie wypelniles formularza. Czy na pewno chcesz isc dalej?"
+        />
+      </div>
+    );
+  }
+}
 
 export default ContactPage;
