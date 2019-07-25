e75111fc9784a6054e1db78529bc704a9146008a
Jacek Maciejak <jacekmaciejak@poczta.fm>
2019-07-25 11:36:37 +0200

    Footer added

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,7 +1,35 @@
 import * as React from "react";
+import { Route } from "react-router-dom";
+import "../styles/Footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <h2>Stopka</h2>
+      <Route
+        path="/"
+        exact
+        render={() => {
+          return (
+            <p>
+              Jestes na <span>stronie glownej</span>
+            </p>
+          );
+        }}
+      />
+      <Route
+        path="/:page"
+        exact
+        render={props => {
+          return (
+            <p>
+              Jestes na <span>{props.match.params.page}</span>
+            </p>
+          );
+        }}
+      />
+    </div>
+  );
 };
 
 export default Footer;
